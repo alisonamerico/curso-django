d25a21883950eddd0a60157a68a7e1270fbdac05
alisonamerico <alison.americo@gmail.com>
2020-05-20 17:00:39 -0300

    atualizada versao no runtime

--- pypro/wsgi.py
+++ pypro/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pypro.settings')
 
