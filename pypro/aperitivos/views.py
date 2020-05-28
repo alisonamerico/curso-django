d654ec79d488673c52afa18a202cb544ade11297
alisonamerico <alison.americo@gmail.com>
2020-05-28 11:18:05 -0300

    Criada pagina para segundo video

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 422595352}
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 422595352},
+        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 423624747},
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
