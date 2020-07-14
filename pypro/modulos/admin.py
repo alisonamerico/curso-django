3475f0d4a7e773fa64c53fd9e27a6e31d505659b
alisonamerico <alison.americo@gmail.com>
2020-07-14 16:22:00 -0300

    testado link dos modulos

--- pypro/modulos/admin.py
+++ pypro/modulos/admin.py
@@ -6,3 +6,4 @@ from ordered_model.admin import OrderedModelAdmin
 @admin.register(Modulo)
 class ModuloAdmin(OrderedModelAdmin):
     list_display = ('titulo', 'publico', 'move_up_down_links')
+    prepopulated_fields = {'slug': ('titulo',)}
