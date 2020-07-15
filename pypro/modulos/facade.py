5a1f1a7b54c7a128d182845605b9b071e4084cec
alisonamerico <alison.americo@gmail.com>
2020-07-15 13:47:08 -0300

    Criada Pagina de Detalhe da Aula

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,4 +1,4 @@
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Aula, Modulo
 from typing import List
 
 
@@ -15,3 +15,7 @@ def encontrar_modulo(slug: str) -> Modulo:
 
 def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
     return list(modulo.aula_set.order_by('order').all())
+
+
+def encontrar_aula(slug: str) -> Aula:
+    return Aula.objects.get(slug=slug)
