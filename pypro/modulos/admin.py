d17713b3737fe7c9ab8e1ddea7831eba030af467
alisonamerico <alison.americo@gmail.com>
2020-07-15 09:47:41 -0300

    Criado relacionamento entre Modulo e Aula

--- pypro/modulos/admin.py
+++ pypro/modulos/admin.py
@@ -1,4 +1,4 @@
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Aula, Modulo
 from django.contrib import admin
 from ordered_model.admin import OrderedModelAdmin
 
@@ -6,4 +6,12 @@ from ordered_model.admin import OrderedModelAdmin
 @admin.register(Modulo)
 class ModuloAdmin(OrderedModelAdmin):
     list_display = ('titulo', 'publico', 'move_up_down_links')
+    prepopulated_fields = {'slug': ('titulo',)}
+
+
+@admin.register(Aula)
+class AulaAdmin(OrderedModelAdmin):
+    list_display = ('titulo', 'modulo', 'order', 'move_up_down_links')
+    list_filter = ('modulo',)
+    ordering = ('modulo', 'order')
     prepopulated_fields = {'slug': ('titulo',)}
